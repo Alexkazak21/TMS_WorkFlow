b25f9ee2b2844d162764da1cbe80d95622e6b0f4
Alexander Kazakevich <sashakazakevich10@gmail.com>
2023-10-14 09:10:10 +0300

    start project

---
+++ lesson5/lesson5_Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace lesson5_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringArray = new List<string>();
+            using (FileStream fstream = File.OpenRead("..\\..\\..\\strings.txt"))
+            {
+                // выделяем массив для считывания данных из файла
+                byte[] buffer = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(buffer, 0, buffer.Length);
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(buffer);
+                stringArray.AddRange(textFromFile.Split("\r\n"));
+            }
+        }
+    }
+}
