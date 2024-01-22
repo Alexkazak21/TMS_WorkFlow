cb3dffceda1356f38e69fa97f0bdf9b70de88a82
Alexander Kazakevich <sashakazakevich10@gmail.com>
2024-01-22 10:45:01 +0300

    Create SummingCollection.cs
    added lesson27

---
+++ lesson27/SummingCollection.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Numerics;
+
+
+namespace CustomCollection;
+
+public class SummingCollection<TSource> : IEnumerable<TSource> where TSource : INumberBase<TSource> 
+{
+    private List<TSource> sum = default;
+
+    public void AddElement(TSource element)
+    {
+        sum.Add(element);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public IEnumerator<TSource> GetEnumerator()
+    {
+        TSource currentSum = default;
+
+        foreach(TSource element in sum)
+        {
+            currentSum += element;
+            yield return currentSum;
+        }
+    }
+}
